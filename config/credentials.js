c81e0a67bef2c226a30212c1cfa6c70c52d48310
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2022-05-17 19:34:20 -0400

    code

---
+++ config/credentials.js
@@ -0,0 +1,12 @@
+{
+  "type": "service_account",
+  "project_id": "logindemo2021-342600",
+  "private_key_id": "0367f29982379c7c260975066873af20a262b302",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDHR5T87EDLk/kF\nIEI8NkZ8qeobHlXa8Iirm09hLkeE0mEr/s0Y8ldMmkUgvM3gODIPHdeXaj91I6fv\nqWfS9UIWkCunNbiFxU3a/593k1ZC4Qi3CMy/nah+GqU1334/rLz8Mw8V2j47Ftx8\nmlahgu7BZn5Zz3I2jCKVcfkIvuX5gkhVf8hvvN60L4jWnInL7KBt+O41Saydbeqa\n87Hl6ZKSVQyW3Xt7KZGdPFvBaYdlPT+xxoFYnecaZKlwl/HTao1dDCLX1sQYmx8O\nFuWNVLSMi2yJOLEPd8QZFTxhLIwZIH73biKdh8JqzBP+PWBQhkId9fRd2qJdrhlI\nosuabnKPAgMBAAECgf8KLeypdPHaYKD1U04O2QWtCcXX641jekhtQJVq/vIyJXlH\nY5U92F5Od7pLVEX032AlZYg7R4DiolIZJwROESV2d790VPtmxjcz5O6PLR7sJEPX\ni9aa33LPm8foWwwX5c6t2DyeQ5tgVaM8a2rGXyTmvC4uQOCoXs7vSYCqLMdrMmB2\ngZhhaTDwPlXcg3au9jq9KyhDgteG2zmRQ3DXccx68Ou9Xse5NFoRErgEeq+a3MsM\ngsIRLpfR++xH7K94b8rTnUylxTFLMmmhMuv5n2V0DH9fIeWSiplq8XmWFD/xOc68\nNSkunOPes5ICysv3ZzvaWBjIDtb6ev3n6ckmC9ECgYEA7wDj+xKmxu+FZ29C2nIh\nuUeNmm0J41y0nWRqgQbftIqytmUoo57aU25mRSpLPr6W1peiGGELZECZTY9HcZxg\n7IQzK5Q2D0t4uinxKREv8uCQGpYGNDkk4RIP2McdMa8AdznJrl+VD26+dCmPCkub\nsINUorHmhxVPgttF0/O+WXUCgYEA1XOCkdbxTSoVtfBU1CdMm7CyoSeWkMsmEbeE\ngyUMZAUylCj+/q9u5gzq2hZAs207OqZDS5VbbDfoetU9lRgJAJYn0QGCtQ3Kxyf8\n0q24l4an8+JY54JzqpqEel2FAqeXX9hzSfoYcE9X1CTs1Li394HDbtYZo5ryvw1W\n9O8/13MCgYEA1HYzNFeBQKleKvPiNzRsPDuaaqPxvVcFDEFe2DJXHTSK58+HT+R/\n6tjiOyv81dsxVbRYGVnzeXxfi24KMbm1A6wFnQDVCcWqnbt5MWug90SfdbLV8wi1\newrGYYunrSoHvPT3D3bSmHR3A+aJzLonvcMRsst4xa6xDv+D1IYCOZ0CgYAqNc18\nSaUuPURN1ljwG1Vq9GZeuBVuTL4agtARnyH0JZL6+aBzfP3RQHyw4/WnhdJQptBo\nAgEyRVwT7ldfRIGDPC5jhojyhOe0upOklfYLAPX1pK0wGvwEoMnR6OyEud+VdJhX\n6L0seIRFz4eGCYFX0LF1GaG4WMvoPBsWFLnrQQKBgAWUwxpWCL1/f6IuBDjEW95Z\nUTBcSEDbGfqLkpfqt9Vm0g5pXc9B0hB0bbq09eaYdITlXp2iYCMWtIzP1EEYpQT7\n6q3ZEwpYQ28JmraJTyJGvbPdWn/kidZ8csnLroQU9TF3ZX+XJnMBk7wiM2fHrDJa\nR7ILrRN+w+gOvV5mpzIy\n-----END PRIVATE KEY-----\n",
+  "client_email": "trinitycookingblog@logindemo2021-342600.iam.gserviceaccount.com",
+  "client_id": "103547751720363673731",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/trinitycookingblog%40logindemo2021-342600.iam.gserviceaccount.com"
+}
